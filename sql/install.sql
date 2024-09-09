076fb80508a1d5b5c21a91f310c2c615ee582b2f
Galen Charlton <gmc@equinoxOLI.org>
2024-09-09 11:51:13 -0400

    add copyright header to install.sql
    Signed-off-by: Galen Charlton <gmc@equinoxOLI.org>

--- sql/install.sql
+++ sql/install.sql
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 Equinox Open Library Initiative, Inc.
+ *
+ * Author: Galen Charlton <gmc@equinoxOLI.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
 DROP SCHEMA IF EXISTS eoli_addons CASCADE;
 
 CREATE SCHEMA eoli_addons;
